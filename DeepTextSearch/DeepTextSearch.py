d98bcb8361a81aec9b476c77507456b151af6fad
youssef <youssef@nomadstory.com>
2023-01-24 01:52:15 +0200

    Adding the capability to add ElAsr name

--- DeepTextSearch/DeepTextSearch.py
+++ DeepTextSearch/DeepTextSearch.py
@@ -5,25 +5,24 @@ import pickle
 import os
 
 
-corpus_list_data = os.path.join('embedding-data/', 'corpus_list_data.pickle')
-corpus_embeddings_data = os.path.join('embedding-data/', 'corpus_embeddings_data.pickle')
-corpus_with_diacritics = os.path.join('embedding-data/', 'corpus_with_diacritics.pickle')
-
-
 class TextEmbedder:
-    def __init__(self):
-        self.corpus_embeddings_data = corpus_embeddings_data
-        self.corpus_list_data = corpus_list_data
+    def __init__(self, elasr_name=None):
+        self.elasr_name = elasr_name
+        self.corpus_embeddings_data = os.path.join('embedding-data/',
+                                                   f'corpus_embeddings_data_{self.elasr_name}.pickle')
+        self.corpus_list_data = os.path.join('embedding-data/', f'corpus_list_data_{self.elasr_name}.pickle')
         self.corpus_list = None
         self.embedder = SentenceTransformer('UBC-NLP/ARBERT')
         self.corpus_embeddings = None
-        self.corpus_with_diacritics = corpus_with_diacritics
+        self.corpus_with_diacritics = os.path.join('embedding-data/',
+                                                   f'corpus_with_diacritics_{self.elasr_name}.pickle')
         if 'embedding-data' not in os.listdir():
             os.makedirs("embedding-data")
-    def embed(self,corpus_list:list, df_diacritics):
+
+    def embed(self, corpus_list: list, df_diacritics):
         self.corpus_list = corpus_list
-        if len(os.listdir("embedding-data/"))==0:
-            self.corpus_embeddings = self.embedder.encode(self.corpus_list,show_progress_bar=True)
+        if len(os.listdir("embedding-data/")) == 0:
+            self.corpus_embeddings = self.embedder.encode(self.corpus_list, show_progress_bar=True)
             df1 = pd.DataFrame()
             df1['features'] = list(self.corpus_embeddings)
             df2 = pd.DataFrame()
@@ -31,14 +30,14 @@ class TextEmbedder:
             pickle.dump(df1, open(self.corpus_embeddings_data, "wb"))
             pickle.dump(df2, open(self.corpus_list_data, "wb"))
             pickle.dump(df_diacritics, open(self.corpus_with_diacritics, "wb"))
-            
+
             print("Embedding data Saved Successfully!")
             print(os.listdir("embedding-data/"))
         else:
             print("Embedding data allready present, Do you want Embed & Save Again? Enter yes or no")
-            flag  = str(input())
+            flag = str(input())
             if flag.lower() == 'yes':
-                self.corpus_embeddings = self.embedder.encode(self.corpus_list,show_progress_bar=True)
+                self.corpus_embeddings = self.embedder.encode(self.corpus_list, show_progress_bar=True)
                 df1 = pd.DataFrame()
                 df1['features'] = list(self.corpus_embeddings)
                 df2 = pd.DataFrame()
@@ -51,8 +50,9 @@ class TextEmbedder:
             else:
                 print("Embedding data allready Present, Please Apply Search!")
                 print(os.listdir("embedding-data/"))
+
     def load_embedding(self):
-        if len(os.listdir("embedding-data/"))==0:
+        if len(os.listdir("embedding-data/")) == 0:
             print("Embedding data Not present, Please Run Embedding First")
         else:
             print("Embedding data Loaded Successfully!")
